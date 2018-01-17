0e9cfd7a26b6f34e21b1d44ac06434949349c599
arolson101 <andrew@olsononline.org>
2018-01-17 11:32:33 -0600

    get rid of unneeded style sheets

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 AppRegistry.registerComponent('App', () => App);
 AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
