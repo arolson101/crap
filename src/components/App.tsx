264d8b1d01c918bc2eb6bc45900609f617789306
arolson101 <andrew@olsononline.org>
2018-02-12 10:43:36 -0600

    basic routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,73 +1,105 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { NavigationScreenComponent, NavigationScreenOptions } from 'react-navigation';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect, RouteComponentProps } from 'react-router-dom';
+import { Bank } from '../state';
 
-const arr: number[] = [];
-for (let i = 0; i < 1000; i++) {
-  arr[i] = i;
-}
+export const banks: Bank[] = [
+  { name: '1st bank', id: '1', _deleted: 0 },
+  { name: '2nd bank', id: '2', _deleted: 0 },
+];
 
-export class Home extends React.Component {
-  static navigationOptions = {
-    title: 'Home title',
-  };
+export const BankLink: React.SFC<{bankId: string}> = ({bankId, children}) => (
+  <Link to={`/bank/${bankId}`}>
+    {children}
+  </Link>
+);
 
-  render() {
-    return (
-      <View>
-        <Text>Home</Text>
-        {arr.map((i) =>
-          <div key={i}>
-            <label>label {i}</label>
-            <input type="text" style={{width: 100}}/>
-            <button>...</button> <button>go</button>
-          </div>
-        )}
-      </View>
-    );
-  }
-}
+export const Sidebar: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>sidebar</Text>
+      {banks.map(bank =>
+        <BankLink key={bank.id} bankId={bank.id}><Text>{bank.name}</Text></BankLink>
+      )}
+    </View>
+  );
+};
 
-export class About extends React.Component {
-  static navigationOptions = {
-    title: 'About',
-  };
+export const Home: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>home tab</Text>
+    </View>
+  );
+};
 
-  render() {
-    return <View><Text>About</Text></View>;
-  }
-}
+export const Accounts: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>accounts tab</Text>
+    </View>
+  );
+};
 
-const Page = <T extends {}>(Screen: NavigationScreenComponent<T, NavigationScreenOptions>) =>
-  ({ children, ...props }: React.Props<T>) => {
-    const navigationOptions: any = Screen.navigationOptions && (typeof Screen.navigationOptions === 'function'
-      ? null
-      : Screen.navigationOptions
-    );
-    return (
-      <View>
-        <Text>{navigationOptions!.title}</Text>
-        <Screen {...props as T}>{children}</Screen>
-      </View>
-    );
-  };
+export const Budgets: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>budgets tab</Text>
+    </View>
+  );
+};
 
-const HomePage = Page(Home);
-const AboutPage = Page(About);
+export const BankTab: React.SFC<RouteComponentProps<{bankId: string}>> = (props) => {
+  return (
+    <View>
+      <Text>bank {props.match.params.bankId}</Text>
+    </View>
+  );
+};
+
+export const Account: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>account tab</Text>
+    </View>
+  );
+};
+
+export const MainView: React.SFC = (props) => {
+  return (
+    <View>
+      <Text>
+        [<Link to="/home">home</Link>]
+        [<Link to="/budgets">budgets</Link>]
+        [<Link to="/accounts">accounts</Link>]
+      </Text>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/budgets" component={Budgets}/>
+        <Route path="/accounts" component={Accounts}/>
+        <Route path="/bank/:bankId" component={BankTab}/>
+        <Route path="/account/:accountId" component={Account}/>
+        <Redirect to="/"/>
+      </Switch>
+    </View>
+  );
+};
+
+export const paths = [
+  '/home',
+  '/accounts',
+  '/budgets',
+  '/account/:accountId',
+  '/budget/:budgetId',
+];
 
 export class App extends React.Component {
   render() {
     return (
       <Router>
         <View>
-          <View>
-            <Link to="/">home</Link>
-            <Link to="/about?1">about 1</Link>
-            <Link to="/about?2">about 2</Link>
-          </View>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
+          <Sidebar/>
+          <MainView/>
         </View>
       </Router>
     );
