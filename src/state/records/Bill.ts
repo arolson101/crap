19c896768a5d571403cd01a26e61463c99a58387
Andrew Olson <andrew@olsononline.org>
2018-02-18 07:50:20 -0600

    add rrule

---
+++ src/state/records/Bill.ts
@@ -0,0 +1,23 @@
+import * as update from 'immutability-helper';
+import { Record } from '../Record';
+
+export interface Bill extends Bill.Props, Record<Bill.Props> {}
+
+export namespace Bill {
+  export interface Props {
+    readonly name: string;
+    readonly group: string;
+    readonly web: string;
+    readonly favicon?: string;
+    readonly notes: string;
+    readonly amount: number;
+    readonly account?: string;
+    readonly category: string;
+    readonly rruleString: string;
+    readonly showAdvanced?: boolean;
+  }
+
+  export type Query = update.Query<Props>;
+  export const table = 'bills';
+  export const schema = { [table]: Record.genSchema() };
+}
