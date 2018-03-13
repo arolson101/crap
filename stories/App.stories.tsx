8225e5a0584be39a3aaba496ae2a6898df560641
arolson101 <andrew@olsononline.org>
2018-03-13 10:12:31 -0500

    static router

--- stories/App.stories.tsx
+++ stories/App.stories.tsx
@@ -1,14 +1,34 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import { StaticRouter } from 'react-router';
 import { configureStore } from '../src/state';
 import { AppComponent } from '../src/components';
 import { dependencies, middlewares } from './storeHelpers';
 
 const store = configureStore(dependencies, middlewares);
 
-storiesOf('App', module)
+storiesOf('App/Login', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
-  .add('not open', () => <AppComponent/>)
-  .add('open', () => <AppComponent isOpen/>)
+  .add('no dbs', () => <AppComponent />)
+  .add('1 dbs', () => <AppComponent />)
+  ;
+
+storiesOf('App/Pages', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('home', () => (
+    <StaticRouter location={{ pathname: '/' }} context={{}}>
+      <AppComponent isOpen />
+    </StaticRouter>
+  ))
+  .add('budgets', () => (
+    <StaticRouter location={{ pathname: '/budgets' }} context={{}}>
+      <AppComponent isOpen />
+    </StaticRouter>
+  ))
+  .add('accounts', () => (
+    <StaticRouter location={{ pathname: '/accounts' }} context={{}}>
+      <AppComponent isOpen />
+    </StaticRouter>
+  ))
   ;
