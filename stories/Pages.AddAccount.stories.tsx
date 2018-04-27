bc997cd1f107a62bc388f3a15b6f7e8027868fdc
arolson101 <andrew@olsononline.org>
2018-04-27 16:37:02 -0500

    knobs for story query

--- stories/Pages.AddAccount.stories.tsx
+++ stories/Pages.AddAccount.stories.tsx
@@ -1,34 +1,20 @@
-import apolloStorybookDecorator from 'apollo-storybook-react';
 import { storiesOf } from '@storybook/react';
+import { boolean } from '@storybook/addon-knobs';
+import apolloStorybookDecorator from 'apollo-storybook-react';
+import faker from 'faker';
 import * as React from 'react';
 import { AccountsCreatePageComponent } from '../src/components/pages/AccountsCreatePage';
 import { Queries } from '../src/db';
+import { AppThemeProvider } from '../src/components/Theme';
 import typeDefs from '../src/db/schema/schema.graphql';
-
-const mocks = {
-  Query: () => {
-    return {
-      banks: () => [
-        {
-          id: '123',
-          name: '1st bank',
-          accounts: [
-            { id: '123a', name: 'checking' },
-            { id: '123b', name: 'savings' },
-          ]
-        }
-      ],
-    };
-  },
-};
 
 const makeQuery = <T extends {}>(data: T) => ({
   data,
-  loading: false,
-  error: undefined,
+  loading: boolean('loading', false),
+  error: boolean('error', false) ? new Error('Error running query') : undefined,
 });
 
-const props = {
+const props = () => ({
   query: makeQuery({
     banks: [
       {
@@ -41,23 +27,24 @@ const props = {
       }
     ]
   })
-};
+});
 
 storiesOf('Pages/AddAccount', module)
+  .addDecorator(story => <AppThemeProvider>{story()}</AppThemeProvider>)
   .addDecorator(
     apolloStorybookDecorator({
       typeDefs,
-      mocks,
+      mocks: {},
     }),
   )
   .add('No banks', () => (
     <AccountsCreatePageComponent
-      {...props}
+      {...props()}
     />
   ))
   .add('Banks', () => (
     <AccountsCreatePageComponent
-      {...props}
+      {...props()}
     />
   ))
   ;
