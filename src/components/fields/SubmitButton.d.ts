704a2b3d7d04022ec9fe8d7662fedde26fce881b
Andrew Olson <andrew@olsononline.org>
2018-05-27 10:28:21 -0500

    SubmitButton split for web

---
+++ src/components/fields/SubmitButton.d.ts
@@ -0,0 +1,8 @@
+import * as web from './SubmitButton.web'
+import * as native from './SubmitButton.native'
+
+declare var _test: typeof web
+declare var _test: typeof native
+
+/// export to get the shape of the module
+export * from './SubmitButton.web'
