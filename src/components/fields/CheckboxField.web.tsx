9098b5cc945f6a8b19baeaba9d0059ec53083d55
Andrew Olson <andrew@olsononline.org>
2018-05-27 10:32:12 -0500

    checkbox use blueprint switch

--- src/components/fields/CheckboxField.web.tsx
+++ src/components/fields/CheckboxField.web.tsx
@@ -1,17 +1,9 @@
-import glamorous from 'glamorous'
 import * as React from 'react'
 import { Field } from 'react-form'
 import { FormattedMessage } from 'react-intl'
-import { Switch } from 'react-native'
+import { Switch } from '@blueprintjs/core'
 import { ctx, ThemeProp } from '../../App'
 import { WrappedField } from './WrappedField'
-
-const StyledSwitch = glamorous(Switch)({},
-  ({ theme }: ThemeProp) => ({
-    marginLeft: 'auto'
-  })
-)
-StyledSwitch.displayName = 'StyledSwitch'
 
 export namespace CheckboxField {
   export interface Props<T = {}> {
@@ -23,13 +15,16 @@ export namespace CheckboxField {
 export const CheckboxField: React.SFC<CheckboxField.Props> =
   ({ field, label }, { intl }: ctx.Intl) => (
     <Field field={field}>
-      {fieldApi =>
-        <WrappedField label={label} fieldApi={fieldApi}>
-          <StyledSwitch
-            onValueChange={(value: boolean) => fieldApi.setValue(value)}
-            value={fieldApi.value}
-          />
-        </WrappedField>
+      {fieldApi => (
+          <WrappedField fieldApi={fieldApi}>
+            <Switch
+              className='pt-align-right'
+              label={intl.formatMessage(label)}
+              checked={fieldApi.value}
+              onChange={e => fieldApi.setValue(e.currentTarget.checked)}
+            />
+          </WrappedField>
+        )
       }
     </Field>
   )
