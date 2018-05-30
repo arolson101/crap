c34fa08595f83f82c1aec585fc4f126525c9bf59
Andrew Olson <andrew@olsononline.org>
2018-05-29 19:07:12 -0500

    fix multiline

--- src/components/fields/TextField.web.tsx
+++ src/components/fields/TextField.web.tsx
@@ -1,21 +1,11 @@
 import glamorous from 'glamorous'
-import { FormGroup, Intent } from '@blueprintjs/core'
+import { FormGroup, Intent, TextArea } from '@blueprintjs/core'
 import * as React from 'react'
 import { Field } from 'react-form'
 import { FormattedMessage } from 'react-intl'
 import { ReturnKeyType, TextInput } from 'react-native'
 import { ThemeProp, ctx } from '../../App'
 import { WrappedField } from './WrappedField'
-
-const StyledTextInput = glamorous.input<ThemeProp & { error: any, textColor?: string }>({},
-  ({ theme, error, textColor }) => ({
-    borderWidth: theme.boxBorderWidth,
-    borderColor: error ? theme.boxBorderColorError : theme.boxBorderColor,
-    fontSize: theme.controlFontSize,
-    color: textColor ? textColor : theme.controlFontColor
-  })
-)
-StyledTextInput.displayName = 'StyledTextInput'
 
 export namespace TextField {
   export interface Props<T = {}> {
@@ -49,14 +39,24 @@ export class TextField extends React.Component<TextField.Props> {
               label={intl.formatMessage(label)}
               labelFor={id}
             >
-              <input
-                id={id}
-                className={'pt-input pt-fill' + (error ? ' pt-intent-danger' : '')}
-                type={secure ? 'password' : 'text'}
-                placeholder={placeholder && intl.formatMessage(placeholder)}
-                onChange={(event) => fieldApi.setValue(event.target.value)}
-                value={fieldApi.value}
-              />
+              {rows && rows > 1 ?
+                <TextArea
+                  id={id}
+                  className={'pt-input pt-fill' + (error ? ' pt-intent-danger' : '')}
+                  placeholder={placeholder && intl.formatMessage(placeholder)}
+                  onChange={(event) => fieldApi.setValue(event.target.value)}
+                  value={fieldApi.value}
+                  rows={rows}
+                />
+                : <input
+                  id={id}
+                  className={'pt-input pt-fill' + (error ? ' pt-intent-danger' : '')}
+                  type={secure ? 'password' : 'text'}
+                  placeholder={placeholder && intl.formatMessage(placeholder)}
+                  onChange={(event) => fieldApi.setValue(event.target.value)}
+                  value={fieldApi.value}
+                />
+              }
             </FormGroup>
           )
         }}
