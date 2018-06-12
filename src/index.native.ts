f7fdabb343a934b561a72dcfe6b589f3a3843b65
arolson101 <andrew@olsononline.org>
2018-06-12 16:59:56 -0500

    playing with react-navigation

--- src/index.native.ts
+++ src/index.native.ts
@@ -3,4 +3,8 @@ import 'intl'
 import { AppRegistry } from 'react-native'
 import Root from './App/Root'
 
+// tslint:disable-next-line:no-duplicate-imports
+import { YellowBox } from 'react-native'
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
+
 AppRegistry.registerComponent('crap', () => Root)
