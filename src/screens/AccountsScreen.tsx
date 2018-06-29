979f6deff368cafcc5f27eed2eddfaa1b6b0dff3
Andrew Olson <andrew@olsononline.org>
2018-06-28 20:38:42 -0500

    add button

--- src/screens/AccountsScreen.tsx
+++ src/screens/AccountsScreen.tsx
@@ -3,71 +3,78 @@ import { defineMessages } from 'react-intl'
 import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router'
 import { compose } from 'recompose'
-import { ctx } from '../App'
 import { ErrorMessage } from '../components/ErrorMessage'
 import { Button, Container, Row, Text } from '../components/layout'
 import { Mutations, Queries } from '../db'
 import { AccountsCreateScreen, AccountsUpdateScreen } from '../modals'
-import { nav, paths } from '../nav'
+import { paths } from '../nav'
 import { actions } from '../redux/actions/index'
 import { AccountPage } from './AccountScreen'
-import { makeScreen } from './Screen'
+import { AddButtonProps, makeScreen } from './Screen'
 
 interface Props {
+}
+
+interface ConnectedProps {
   query: Queries.Banks
   deleteBank: Mutations.DeleteBank
   deleteAccount: Mutations.DeleteAccount
   modalAccountCreate: () => any
+  navBank: (bankId: string) => any
+  navAccount: (accountId: string) => any
 }
 
-export const AccountsScreenComponent: React.SFC<Props> = (props, { router }: ctx.Router) => {
-  if (props.query.loading) {
-    return null
+class AccountsScreenComponent extends React.Component<Props & ConnectedProps & AddButtonProps> {
+  componentDidMount () {
+    this.props.setAdd(this.props.modalAccountCreate)
   }
 
-  if (props.query.error) {
-    return <ErrorMessage error={props.query.error} />
+  componentDidUpdate () {
+    this.props.setAdd(this.props.modalAccountCreate)
   }
 
-  return (
-    <>
-      <Switch>
-        <Route path={paths.account.create} component={AccountsCreateScreen} />
-        <Route path={paths.account.update} component={AccountsUpdateScreen} />
-        <Route path={paths.account.view} component={AccountPage} />
-        <Route>
-          <>
-            <Text>Accounts page</Text>
-            {props.query.data.banks.map(bank =>
-              <Container key={bank.id}>
-                <Text>{bank.name}</Text>
-                <Row>
-                  <Button title='edit bank' onPress={() => router.history.push(nav.bankUpdate(bank.id))} />
-                  <Button title='delete bank' onPress={() => props.deleteBank.execute({ variables: { bankId: bank.id } })} />
-                  <Button title='add account' onPress={() => router.history.push(nav.accountCreate(bank.id))} />
-                </Row>
-                {bank.accounts.map(account =>
-                  <Row key={account.id}>
-                    <Button
-                      title={account.name}
-                      onPress={() => router.history.push(nav.accountView(bank.id, account.id))}
-                    />
-                    <Button
-                      title='delete'
-                      onPress={() => props.deleteAccount.execute({ variables: { accountId: account.id } })}
-                    />
+  render () {
+    if (this.props.query.loading) {
+      return null
+    }
+
+    if (this.props.query.error) {
+      return <ErrorMessage error={this.props.query.error} />
+    }
+
+    return (
+      <>
+        <Switch>
+          <Route path={paths.account.create} component={AccountsCreateScreen} />
+          <Route path={paths.account.update} component={AccountsUpdateScreen} />
+          <Route path={paths.account.view} component={AccountPage} />
+          <Route>
+            <>
+              <Text>Accounts page</Text>
+              {this.props.query.data.banks.map(bank =>
+                <Container key={bank.id}>
+                  <Text>{bank.name}</Text>
+                  <Row>
+                    <Button title='bank' onPress={() => this.props.navBank(bank.id)} />
                   </Row>
-                )}
-              </Container>
-            )}
-            <Button onPress={props.modalAccountCreate} title='add bank' />
-          </>
-        </Route>
-      </Switch>
-    </>
-  )
+                  {bank.accounts.map(account =>
+                    <Row key={account.id}>
+                      <Button
+                        title={account.name}
+                        onPress={() => this.props.navAccount(account.id)}
+                      />
+                    </Row>
+                  )}
+                </Container>
+              )}
+              <Button onPress={this.props.modalAccountCreate} title='add bank' />
+            </>
+          </Route>
+        </Switch>
+      </>
+    )
+  }
 }
-AccountsScreenComponent.contextTypes = ctx.router
 
 const messages = defineMessages({
   title: {
@@ -77,7 +84,7 @@ const messages = defineMessages({
 })
 
 export const AccountsScreen = compose(
-  makeScreen({ title: messages.title }),
+  makeScreen({ title: messages.title, addButton: 'onAdd' }),
   Queries.withBanks('query'),
   Mutations.withDeleteBank('deleteBank'),
   Mutations.withDeleteAccount('deleteAccount'),
--- src/screens/Screen.tsx
+++ src/screens/Screen.tsx
@@ -1,13 +1,21 @@
-import { Body, Container } from 'native-base'
+import { Body, Container, Icon } from 'native-base'
 import platform from 'native-base/dist/src/theme/variables/platform'
 import * as React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { NavigationParams, NavigationScreenComponent } from 'react-navigation'
+import HeaderButtons from 'react-navigation-header-buttons'
 import { ScreenProps } from '../App/App.native'
+
+export interface AddButtonProps {
+  setAdd: (callback: () => any) => any
+}
 
 interface Params {
   title: FormattedMessage.MessageDescriptor,
+  addButton?: string
 }
 
 const styles = StyleSheet.create({
@@ -35,15 +43,31 @@ export type ScreenComponent<P = any> = NavigationScreenComponent<NavigationParam
 
 export const makeScreen = (params: Params) => {
   const { title } = params
+  console.log('makeScreen')
+  let onAdd = () => { console.warn('no add function') }
+  const setAdd = (addfcn: () => any ) => { onAdd = addfcn }
+  const moreProps = params.addButton ? { setAdd } : {}
+
+  const IconComponent = Platform.OS === 'ios' ? Ionicons : MaterialIcons
+  const addIconName = Platform.OS === 'ios' ? 'ios-add' : 'add'
 
   return <P extends object>(Component: React.ComponentType<P>) => {
     const nav: ScreenComponent<P> = ((props) => (
       <Container>
         <Body>
-          <Component {...props} />
+          <Component {...props} {...moreProps} />
         </Body>
       </Container>
     )) as NavigationScreenComponent<NavigationParams, {}, P> as any
+
+    const addNavigationOptions = params.addButton ? ({
+      headerRight: (
+        <HeaderButtons IconComponent={IconComponent} iconSize={platform.iconHeaderSize} color={platform.toolbarBtnColor}>
+          <HeaderButtons.Item title='add' iconName={addIconName} onPress={() => onAdd()} />
+        </HeaderButtons>
+      )
+    }) : ({})
+
     nav.navigationOptions = ({ navigation, screenProps }) => {
       const { intl } = screenProps as ScreenProps
       return ({
@@ -52,6 +76,7 @@ export const makeScreen = (params: Params) => {
         headerBackTitleStyle: styles.headerBackTitleStyle,
         headerTintColor: platform.toolbarBtnColor,
         headerTitle: intl.formatMessage(title),
+        ...addNavigationOptions,
       })
     }
     nav.title = title
