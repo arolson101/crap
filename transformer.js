0e08b8dce6fa2236f51f9e3d376d9fac579d360d
Andrew Olson <andrew@olsononline.org>
2018-07-09 09:52:05 -0500

    dependency update, not working yet

--- transformer.js
+++ transformer.js
@@ -1,9 +1,14 @@
+const gqlLoader = require('graphql-tag/loader');
 const tsTransformer = require("react-native-typescript-transformer");
-const gqlTransformer = require("react-native-graphql-transformer");
+// const gqlTransformer = require("react-native-graphql-transformer");
+
+const gqlTransform = gqlLoader.bind({
+  cacheable: () => null,
+});
 
 module.exports.transform = function({ src, filename, options }) {
   if (filename.endsWith(".graphql") || filename.endsWith(".gql")) {
-    return gqlTransformer.transform({ src, filename, options });
+    return gqlTransform({ src, filename, options });
   }
   return tsTransformer.transform({ src, filename, options });
 };
