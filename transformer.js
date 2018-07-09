6d6a42eee268d9441ba0f4e4992c389245fb1f6d
arolson101 <andrew@olsononline.org>
2018-07-09 14:39:04 -0500

    fix babel rollback

--- transformer.js
+++ transformer.js
@@ -1,14 +1,14 @@
-const gqlLoader = require('graphql-tag/loader');
+// const gqlLoader = require('graphql-tag/loader');
 const tsTransformer = require("react-native-typescript-transformer");
-// const gqlTransformer = require("react-native-graphql-transformer");
+const gqlTransformer = require("react-native-graphql-transformer");
 
-const gqlTransform = gqlLoader.bind({
-  cacheable: () => null,
-});
+// const gqlTransform = gqlLoader.bind({
+//   cacheable: () => null,
+// });
 
 module.exports.transform = function({ src, filename, options }) {
   if (filename.endsWith(".graphql") || filename.endsWith(".gql")) {
-    return gqlTransform({ src, filename, options });
+    return gqlTransformer.transform({ src, filename, options });
   }
   return tsTransformer.transform({ src, filename, options });
 };
