c9869aa546372d4b58343c95aa252d626fe27a02
arolson101 <andrew@olsononline.org>
2018-07-11 13:16:21 -0500

    import as namespace instead of individually

--- src/db/queries/index.tsx
+++ src/db/queries/index.tsx
@@ -1,20 +1,20 @@
 import { QueryDesc } from './makeQuery'
-import { Account, Accounts, Bank, Banks, Dbs } from './queries-types'
+import * as QT from './queries-types'
 import * as GQL from './queries.graphql'
 
 export namespace Queries {
-  export type Account = Account.Query
-  export const account: QueryDesc<Account.Variables, Account.Query> = { query: GQL.Account }
+  export type Account = QT.Account.Query
+  export const account: QueryDesc<QT.Account.Variables, QT.Account.Query> = { query: GQL.Account }
 
-  export type Accounts = Accounts.Query
-  export const accounts: QueryDesc<Accounts.Variables, Accounts.Query> = { query: GQL.Accounts }
+  export type Accounts = QT.Accounts.Query
+  export const accounts: QueryDesc<QT.Accounts.Variables, QT.Accounts.Query> = { query: GQL.Accounts }
 
-  export type Bank = Bank.Query
-  export const bank: QueryDesc<Bank.Variables, Bank.Query> = { query: GQL.Bank }
+  export type Bank = QT.Bank.Query
+  export const bank: QueryDesc<QT.Bank.Variables, QT.Bank.Query> = { query: GQL.Bank }
 
-  export type Banks = Banks.Query
-  export const banks: QueryDesc<Banks.Variables, Banks.Query> = { query: GQL.Banks }
+  export type Banks = QT.Banks.Query
+  export const banks: QueryDesc<QT.Banks.Variables, QT.Banks.Query> = { query: GQL.Banks }
 
-  export type Dbs = Dbs.Query
-  export const dbs: QueryDesc<Dbs.Variables, Dbs.Query> = { query: GQL.Dbs }
+  export type Dbs = QT.Dbs.Query
+  export const dbs: QueryDesc<QT.Dbs.Variables, QT.Dbs.Query> = { query: GQL.Dbs }
 }
